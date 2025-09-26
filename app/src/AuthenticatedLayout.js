30d2eb217648d15b5542ee8ed2c23fd748ec73ba
Shivam Sahu <51310713+seevam@users.noreply.github.com>
2025-09-26 13:48:08 +0530

    Update AuthenticatedLayout.js

--- app/src/AuthenticatedLayout.js
+++ app/src/AuthenticatedLayout.js
@@ -2,11 +2,17 @@ import React from 'react';
 import SharedNavigation from './SharedNavigation';
 
 const AuthenticatedLayout = ({ children }) => {
+  console.log('AuthenticatedLayout rendering'); // Debug log
+  
   return (
-    <div className="min-h-screen bg-gray-50">
+    <>
       {children}
       <SharedNavigation />
-    </div>
+      {/* Debug element to make sure layout is working */}
+      <div className="fixed bottom-20 right-4 bg-red-500 text-white p-2 text-xs rounded" style={{ zIndex: 999 }}>
+        Layout Active
+      </div>
+    </>
   );
 };
 
